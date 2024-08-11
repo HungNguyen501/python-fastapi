89103c61caf8dd4d61f57f8fafb57c7e85a908db
mrroot501 <hungnguyendinh.501@gmail.com>
2024-08-11 22:12:28 +0700

    update test_settings

--- src/common/tests/test_settings.py
+++ src/common/tests/test_settings.py
@@ -1,7 +1,11 @@
 """ Unit test for settings module """
+from unittest.mock import patch, call
+
 from src.common.settings import get_settings
 
 
-def test_get_settings():
+@patch(target="src.common.settings.Settings")
+def test_get_settings(mock_settings):
     """Test get_settings functions"""
-    assert get_settings().Config.env_file == ".env"
+    get_settings()
+    assert mock_settings.call_args == call()
