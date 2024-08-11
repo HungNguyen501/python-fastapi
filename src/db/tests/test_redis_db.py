74547a859958adf65c945a2e68e72e6783594e95
mrroot501 <hungnguyendinh.501@gmail.com>
2024-08-11 22:30:24 +0700

    update unit tests

--- src/db/tests/test_redis_db.py
+++ src/db/tests/test_redis_db.py
@@ -22,19 +22,19 @@ class RedisMock:
 
 
 @pytest.mark.asyncio
-@patch(target="src.db.database.get_settings")
+@patch(target="src.db.redis_db.get_settings")
 @patch(target="src.db.redis_db.Redis", return_value=RedisMock())
 @patch(target="src.db.redis_db.ConnectionPool", side_effect=AsyncMock())
 async def test_redis_pool(mock_connection_pool, mock_redis, mock_settings, *_):
     """Test RedisPool class"""
-    mock_settings.return_value.REDIS_HOST.__str__.return_value = "redis_local"
-    mock_settings.return_value.REDIS_PORT.__str__.return_value = "911"
+    mock_settings.return_value.REDIS_HOST = "redis_local"
+    mock_settings.return_value.REDIS_PORT = 911
     mock_pool = AsyncMock()
     async with RedisPool() as pool:
         pool._pool = mock_pool
         await pool.set(key="ping", value="pong")
         await pool.get(key="ping") == "pong"
-    assert mock_connection_pool.call_args == call(host='127.0.0.1', port=6379, decode_responses=True, db=0)
+    assert mock_connection_pool.call_args == call(host='redis_local', port=911, decode_responses=True, db=0)
     assert mock_redis.call_args == call(connection_pool=mock_pool)
 
 
--- src/services/tests/test_auth_service.py
+++ src/services/tests/test_auth_service.py
@@ -11,7 +11,7 @@ from src.common.exceptions import CredentialsException
 
 @pytest.mark.asyncio
 @freeze_time("2024-01-01")
-@patch(target="src.db.database.get_settings")
+@patch(target="src.services.auth_service.get_settings")
 async def test_auth_service_class(mock_settings):
     """Test AuthService class"""
     mock_user_service = AsyncMock()
@@ -22,8 +22,9 @@ async def test_auth_service_class(mock_settings):
     result = await auth_service.create_user(data=mock_data)
     assert result.message == "created"
     # Test authenticate function
-    mock_settings.return_value.SECRET_KEY.__str__.return_value = "dum_key"
-    mock_settings.return_value.ALGORITHM.__str__.return_value = "HS256"
+    mock_settings.return_value.SECRET_KEY = "dum_key"
+    mock_settings.return_value.ALGORITHM = "HS256"
+    mock_settings.return_value.ACCESS_TOKEN_EXPIRE_MINUTES = 5
     auth_service.redis_pool.get.side_effect = [None, json.dumps({"uuid": "-1"}), json.dumps({"uuid": "-1"})]
     with pytest.raises(CredentialsException):
         await auth_service.authenticate(username="bob", password="-1")
@@ -33,13 +34,13 @@ async def test_auth_service_class(mock_settings):
     with pytest.raises(CredentialsException):
         await auth_service.authenticate(username="bob", password="0")
     result = await auth_service.authenticate(username="bob", password="-1")
-    assert result.access_token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MDY3NTAwLjB9.2H6JFMDS3E7UME5nmhtTc07BruyTxqY5508TVWQlwjk"  # noqa: E501 # pylint: disable=line-too-long
+    assert result.access_token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MDY3NTAwLjB9.Dkdw5jLf9Vq5lov08gKz2RkubC52V2ccVIFj19HAc1Q"  # noqa: E501 # pylint: disable=line-too-long
     assert result.token_type == "bearer"
 
 
 @pytest.mark.asyncio
 @freeze_time("2024-01-02")
-@patch(target="src.db.database.get_settings")
+@patch(target="src.services.auth_service.get_settings")
 async def test_get_current_user_uuid(mock_settings):
     """Test get_current_user_uuid function"""
     # In case: Invalid credentials
@@ -47,17 +48,18 @@ async def test_get_current_user_uuid(mock_settings):
         await get_current_user_uuid(token="-1")
     assert exc.value.detail == "Invalid credentials"
     # In case: UUID is null
-    mock_settings.return_value.SECRET_KEY.__str__.return_value = "dum_key"
-    mock_settings.return_value.ALGORITHM.__str__.return_value = "HS256"
+    mock_settings.return_value.SECRET_KEY = "dum_key"
+    mock_settings.return_value.ALGORITHM = "HS256"
+    mock_settings.return_value.ACCESS_TOKEN_EXPIRE_MINUTES = 5
     with pytest.raises(CredentialsException) as exc:
         await get_current_user_uuid(
-            token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhYmMiOiItMSIsImV4cGlyZXNfYXQiOjE3MDQwNjc1MDAuMH0.xtZqOyPMRXfbrSy3Nym64kJ0HHK3PO97EHyFULvdMUY")  # noqa: E501 # pylint: disable=line-too-long
+            token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhYmMiOiItMSIsImV4cGlyZXNfYXQiOjE3MDQwNjc1MDAuMH0.ICeoyZIKcU_qRMPmvrsJmzvl3-IUyzrHCDpCJ1sVDVc")  # noqa: E501 # pylint: disable=line-too-long
     assert exc.value.detail == "Token does not include UUID"
     # In case: Toke expires
     with pytest.raises(CredentialsException) as exc:
         await get_current_user_uuid(
-            token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MDY3NTAwLjB9.2H6JFMDS3E7UME5nmhtTc07BruyTxqY5508TVWQlwjk")  # noqa: E501 # pylint: disable=line-too-long
+            token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MDY3NTAwLjB9.Dkdw5jLf9Vq5lov08gKz2RkubC52V2ccVIFj19HAc1Q")  # noqa: E501 # pylint: disable=line-too-long
     assert exc.value.detail == "Token expires"
     # In case: successful
     assert "-1" == await get_current_user_uuid(
-        token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MTUzOTAwLjB9.ZL2Y8zc0xpG43NI4tLsDKC9okKtUYtAbfR7rq2jgd9w")  # noqa: E501 # pylint: disable=line-too-long
+        token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiLTEiLCJleHBpcmVzX2F0IjoxNzA0MTUzOTAwLjB9.rXSSBk_3W2_6QDgtdULrp3miTm_Wecg9rY48zcQSJXk")  # noqa: E501 # pylint: disable=line-too-long
